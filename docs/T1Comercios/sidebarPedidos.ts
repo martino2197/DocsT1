5bfd94a9ab9fb72842b29eb0c796c467aa463d51
T1 Pagos 4 <t1pagos4@MacBook-Air-de-T1.local>
2024-06-10 18:17:57 -0600

    T1Comercios Docs Pedidos_v3

--- docs/T1Comercios/sidebarPedidos.ts
+++ docs/T1Comercios/sidebarPedidos.ts
@@ -8,13 +8,61 @@ const sidebar: SidebarsConfig = {
     },
     {
       type: "category",
-      label: "Pedidos",
+      label: "Lista pedidos Pedidos",
       items: [
         {
           type: "doc",
           id: "T1Comercios/get-orders-full",
           label: "Listado de pedidos de un comercio.",
           className: "api-method get",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Anexos",
+      items: [
+        {
+          type: "doc",
+          id: "T1Comercios/download-orderservice-pdf",
+          label: "Descarga la Orden de compra  de un pedido en formato PDF",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "T1Comercios/download-order-guide-pdf",
+          label: "Descarga la guía un pedido en formato PDF",
+          className: "api-method get",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Guías",
+      items: [
+        {
+          type: "doc",
+          id: "T1Comercios/upload-order-guide",
+          label: "Carga la guía de un pedido",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "T1Comercios/upload-status",
+          label: "uploadStatus",
+          className: "api-method post",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Cancelar pedidos",
+      items: [
+        {
+          type: "doc",
+          id: "T1Comercios/cancel-order",
+          label: "Cancelación de una partida de un pedido",
+          className: "api-method post",
         },
       ],
     },
