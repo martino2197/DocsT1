20d505753c6641a8a4ef6878f2721149e2138880
T1 Pagos 4 <t1pagos4@MacBook-Air-de-T1.local>
2024-06-17 12:48:51 -0600

    T1Comercios Docs Productos_V6

---
+++ docs/T1Comercios/sidebarProducts.ts
@@ -0,0 +1,66 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebar: SidebarsConfig = {
+  apisidebar: [
+    {
+      type: "doc",
+      id: "T1Comercios/documentacion-api-v-2-t-1-comercios",
+    },
+    {
+      type: "category",
+      label: "Productos",
+      items: [
+        {
+          type: "doc",
+          id: "T1Comercios/creacrion-de-un-producto-en-la-plataforma-t-1-comercios",
+          label: "Creación de un producto en la plataforma T1comercios",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "T1Comercios/listado-de-productos-del-comercio",
+          label: "Listado de productos del comercio",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "T1Comercios/actualizacion-de-informacion-de-un-proudcto",
+          label: "Actualización de información de un producto \n",
+          className: "api-method patch",
+        },
+        {
+          type: "doc",
+          id: "T1Comercios/obtener-informacion-del-producto",
+          label: "Obtener información del producto",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "T1Comercios/eliminacion-del-producto-en-t-1-comercios",
+          label: "Eliminación del producto en T1comercios",
+          className: "api-method delete",
+        },
+        {
+          type: "doc",
+          id: "T1Comercios/pausar-publicacion",
+          label: "Pausar publicación",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "T1Comercios/actuvar-publicacion",
+          label: "Activar publicación",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "T1Comercios/obtiene-informacion-de-las-variaciones-de-un-producto",
+          label: "Obtiene información de las variaciones de un producto",
+          className: "api-method get",
+        },
+      ],
+    },
+  ],
+};
+
+export default sidebar.apisidebar;
