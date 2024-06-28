c23097bbb94279cfce902aae975196b26e98977e
T1 Pagos 4 <t1pagos4@MacBook-Air-de-T1.local>
2024-06-28 15:23:03 -0600

    T1Docs LandingPage Footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,19 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
 
 import styles from "./index.module.css";
-import { Card, Typography } from "@mui/material";
+import { Button, Card, IconButton, Typography } from "@mui/material";
 
 import React from "react";
 
 const T1Logo = require("@site/static/img/T1Logo.svg").default;
+const T1EnviosLogo = require("@site/static/img/T1EnviosLogo.svg").default;
+const T1PagosLogo = require("@site/static/img/T1PagosLogos.svg").default;
+const T1ComerciosLogo = require("@site/static/img/T1ComerciosLogo.svg").default;
+const T1PaginasLogo = require("@site/static/img/T1PaginasLogos.svg").default;
+
+const FacebookIcon = require("@site/static/img/facebookIcon.svg").default;
+const InstagramIcon = require("@site/static/img/instagramIcon.svg").default;
+const LinkedinIcon = require("@site/static/img/linkedinIcon.svg").default;
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -63,6 +71,475 @@ function HomepageHeader() {
   );
 }
 
+function HomepageFooter() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero", styles.heroBanner)}>
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          gap: "4rem",
+        }}
+      >
+        <div
+          className={clsx("col col--2")}
+          style={{
+            display: "flex",
+            maxWidth: "120px",
+            minHeight: "120px",
+            borderRadius: "50%",
+          }}
+        >
+          <T1Logo style={{ minWidth: 60, minHeight: 60 }} role="img" />
+        </div>
+        <div
+          className={clsx("col col--2")}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "justify",
+          }}
+        >
+          <Typography sx={{ color: "#DB3B2B" }} variant="subtitle1">
+            Documentación
+          </Typography>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+            }}
+          >
+            <Button
+              sx={{
+                fontSize: 14,
+                borderRadius: 3,
+                textTransform: "none",
+                padding: 0,
+              }}
+              size="small"
+              variant="text"
+            >
+              <Link
+                to="/docs/T1Pagos/t-1-pagos"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                T1Pagos
+              </Link>
+            </Button>
+            <Button
+              sx={{
+                fontSize: 14,
+                borderRadius: 3,
+                textTransform: "none",
+                padding: 0,
+              }}
+              size="small"
+              variant="text"
+            >
+              <Link
+                to="/docs/T1Comercios/claro-authentication-api"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                T1Comercios
+              </Link>
+            </Button>
+            <Button
+              sx={{
+                fontSize: 14,
+                borderRadius: 3,
+                textTransform: "none",
+                padding: 0,
+              }}
+              size="small"
+              variant="text"
+            >
+              <Link
+                to="/docs/T1Envíos/t-1-envios"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                T1Envios
+              </Link>
+            </Button>
+          </div>
+        </div>
+        <div
+          className={clsx("col col--1")}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "justify",
+          }}
+        >
+          <Typography sx={{ color: "#DB3B2B" }} variant="subtitle1">
+            Blog
+          </Typography>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+            }}
+          >
+            <Button
+              sx={{
+                fontSize: 14,
+                borderRadius: 3,
+                textTransform: "none",
+                padding: 0,
+                display: "flex",
+                justifyContent: "start",
+              }}
+              size="small"
+              variant="text"
+            >
+              <Link to="/blog " color="#333333" style={{ color: "#333333" }}>
+                Blog
+              </Link>
+            </Button>
+          </div>
+        </div>
+        <div
+          className={clsx("col col--1")}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+        >
+          <IconButton
+            sx={{
+              fontSize: 14,
+              textTransform: "none",
+              padding: "4px",
+              display: "flex",
+              justifyContent: "start",
+            }}
+            aria-label="T1Envios"
+          >
+            <Link to="https://t1envios.com/">
+              <T1EnviosLogo style={{ maxWidth: 62, minHeight: 15.2 }} />
+            </Link>
+          </IconButton>
+
+          <div
+            style={{
+              display: "flex",
+            }}
+          >
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="facebook"
+            >
+              <Link
+                to="https://www.facebook.com/t1envios"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <FacebookIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="instagram"
+            >
+              <Link
+                to="https://www.instagram.com/t1envios"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <InstagramIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="linkedin"
+            >
+              <Link
+                to="https://www.linkedin.com/company/t1envios/"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <LinkedinIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+          </div>
+        </div>
+        <div
+          className={clsx("col col--1")}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+        >
+          <IconButton
+            sx={{
+              fontSize: 14,
+              textTransform: "none",
+              padding: "4px",
+              display: "flex",
+              justifyContent: "start",
+            }}
+            aria-label="T1Pagos"
+          >
+            <Link to="https://www.t1pagos.com/">
+              <T1PagosLogo style={{ maxWidth: 62, minHeight: 18.7 }} />
+            </Link>
+          </IconButton>
+          <div
+            style={{
+              display: "flex",
+            }}
+          >
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="facebook"
+            >
+              <Link
+                to="https://www.facebook.com/profile.php?id=100086226317597"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <FacebookIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="instagram"
+            >
+              <Link
+                to="https://www.instagram.com/t1pagos_/"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <InstagramIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="linkedin"
+            >
+              <Link
+                to="https://www.linkedin.com/company/t1pagos/"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <LinkedinIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+          </div>
+        </div>
+        <div
+          className={clsx("col col--1")}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+        >
+          <IconButton
+            sx={{
+              fontSize: 14,
+              textTransform: "none",
+              padding: "4px",
+              display: "flex",
+              justifyContent: "start",
+            }}
+            aria-label="T1Comercios"
+          >
+            <Link to="https://www.t1comercios.com/">
+              <T1ComerciosLogo style={{ width: 87.5, minHeight: 15.5 }} />
+            </Link>
+          </IconButton>
+          <div
+            style={{
+              display: "flex",
+            }}
+          >
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="facebook"
+            >
+              <Link
+                to="https://www.facebook.com/t1comercios"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <FacebookIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="instagram"
+            >
+              <Link
+                to="https://www.instagram.com/t1comercios/"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <InstagramIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="linkedin"
+            >
+              <Link
+                to="https://www.linkedin.com/company/t1comercios/"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <LinkedinIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+          </div>
+        </div>
+        <div
+          className={clsx("col col--1")}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+        >
+          <IconButton
+            sx={{
+              fontSize: 14,
+              textTransform: "none",
+              padding: "4px",
+              display: "flex",
+              justifyContent: "start",
+            }}
+            aria-label="T1Paginas"
+          >
+            <Link to="https://t1paginas.com/">
+              <T1PaginasLogo style={{ width: 72, minHeight: 15.5 }} />
+            </Link>
+          </IconButton>
+          <div
+            style={{
+              display: "flex",
+            }}
+          >
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="facebook"
+            >
+              <Link
+                to="https://www.facebook.com/t1paginas"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <FacebookIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="instagram"
+            >
+              <Link
+                to="https://www.instagram.com/t1paginas/"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <InstagramIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+            <IconButton
+              sx={{
+                fontSize: 14,
+                textTransform: "none",
+                padding: "4px",
+                display: "flex",
+                justifyContent: "start",
+              }}
+              aria-label="linkedin"
+            >
+              <Link
+                to="https://www.linkedin.com/company/t1paginas/"
+                color="#333333"
+                style={{ color: "#333333" }}
+              >
+                <LinkedinIcon style={{ width: 16, height: 16 }} />
+              </Link>
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -74,6 +551,7 @@ export default function Home(): JSX.Element {
       <main>
         <HomepageFeatures />
       </main>
+      <HomepageFooter />
     </Layout>
   );
 }
