fa811a96166a6a0958183414e9eec29e88633904
T1 Pagos 4 <t1pagos4@MacBook-Air-de-T1.local>
2024-07-01 16:04:46 -0600

    T1Docs fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -544,7 +544,7 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title} Docs`}
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
