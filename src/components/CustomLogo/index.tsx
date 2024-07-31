d3c7d09385426fc6dd6e3734cb3706a3870be06a
T1 Pagos 4 <t1pagos4@MacBook-Air-de-T1.local>
2024-07-30 19:36:59 -0600

    T1Docs sidebar dynamic logo dark mode fix bug

--- src/components/CustomLogo/index.tsx
+++ src/components/CustomLogo/index.tsx
@@ -1,50 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from '@docusaurus/router';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const CustomLogo: React.FC = () => {
   const location = useLocation();
-  const [theme, setTheme] = useState(document.documentElement.getAttribute('data-theme') || 'light');
 
-  useEffect(() => {
-    // Función para manejar el cambio de tema
-    const handleThemeChange = () => {
-      const currentTheme = document.documentElement.getAttribute('data-theme');
-      setTheme(currentTheme || 'light');
-    };
+  return (
+    <BrowserOnly>
+      {() => {
+        const [theme, setTheme] = useState(
+          document.documentElement.getAttribute('data-theme') || 'light'
+        );
 
-    // Añade un observador de mutaciones para detectar cambios en el atributo de tema
-    const observer = new MutationObserver(handleThemeChange);
-    observer.observe(document.documentElement, {
-      attributes: true,
-      attributeFilter: ['data-theme'],
-    });
+        useEffect(() => {
+          // Función para manejar el cambio de tema
+          const handleThemeChange = () => {
+            const currentTheme = document.documentElement.getAttribute('data-theme');
+            setTheme(currentTheme || 'light');
+          };
 
-    // Inicializa el tema al montar el componente
-    handleThemeChange();
+          // Añade un observador de mutaciones para detectar cambios en el atributo de tema
+          const observer = new MutationObserver(handleThemeChange);
+          observer.observe(document.documentElement, {
+            attributes: true,
+            attributeFilter: ['data-theme'],
+          });
 
-    // Limpia el observador cuando el componente se desmonta
-    return () => {
-      observer.disconnect();
-    };
-  }, []); // Solo se ejecuta una vez al montar
+          // Inicializa el tema al montar el componente
+          handleThemeChange();
 
-  useEffect(() => {
-    // Selecciona la imagen del logo utilizando un selector de clase
-    const logoImg = document.querySelector<HTMLImageElement>('.navbar__logo img');
-    if (logoImg) {
-      let logoSrc;
-      if (location.pathname.startsWith('/docs/T1Comercios')) {
-        theme === 'light' ? logoSrc = '/img/T1ComerciosLogo.svg' : logoSrc = '/img/T1ComerciosLogoDark.svg';
-      } else if (location.pathname.startsWith('/docs/T1Pagos')) {
-        theme === 'light' ? logoSrc = '/img/T1PagosLogos.svg' : logoSrc = '/img/T1PagosLogoDark.svg';
-      } else {
-        theme === 'light' ? logoSrc = '/img/t1envios-logo.svg' : logoSrc = '/img/T1EnviosLogoDark.svg';
-      }
-      logoImg.src = logoSrc;
-    }
-  }, [location.pathname, theme]); // Ejecuta cuando cambia la ruta o el tema
+          // Limpia el observador cuando el componente se desmonta
+          return () => {
+            observer.disconnect();
+          };
+        }, []); // Solo se ejecuta una vez al montar
 
-  return null;
+        useEffect(() => {
+          // Selecciona la imagen del logo utilizando un selector de clase
+          const logoImg = document.querySelector<HTMLImageElement>('.navbar__logo img');
+          if (logoImg) {
+            let logoSrc;
+            if (location.pathname.startsWith('/docs/T1Comercios')) {
+              theme === 'light'
+                ? (logoSrc = '/img/T1ComerciosLogo.svg')
+                : (logoSrc = '/img/T1ComerciosLogoDark.svg');
+            } else if (location.pathname.startsWith('/docs/T1Pagos')) {
+              theme === 'light'
+                ? (logoSrc = '/img/T1PagosLogos.svg')
+                : (logoSrc = '/img/T1PagosLogoDark.svg');
+            } else {
+              theme === 'light'
+                ? (logoSrc = '/img/t1envios-logo.svg')
+                : (logoSrc = '/img/T1EnviosLogoDark.svg');
+            }
+            logoImg.src = logoSrc;
+          }
+        }, [location.pathname, theme]); // Ejecuta cuando cambia la ruta o el tema
+
+        return null;
+      }}
+    </BrowserOnly>
+  );
 };
 
 export default CustomLogo;
