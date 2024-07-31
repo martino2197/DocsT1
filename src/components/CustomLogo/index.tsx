35793e6e47356dca299e84f28f75954ff242348d
T1 Pagos 4 <t1pagos4@MacBook-Air-de-T1.local>
2024-07-30 18:51:21 -0600

    T1Docs sidebar dynamic logo dark mode

--- src/components/CustomLogo/index.tsx
+++ src/components/CustomLogo/index.tsx
@@ -1,10 +1,32 @@
-// src/components/CustomLogo.tsx
-
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from '@docusaurus/router';
 
 const CustomLogo: React.FC = () => {
   const location = useLocation();
+  const [theme, setTheme] = useState(document.documentElement.getAttribute('data-theme') || 'light');
+
+  useEffect(() => {
+    // Función para manejar el cambio de tema
+    const handleThemeChange = () => {
+      const currentTheme = document.documentElement.getAttribute('data-theme');
+      setTheme(currentTheme || 'light');
+    };
+
+    // Añade un observador de mutaciones para detectar cambios en el atributo de tema
+    const observer = new MutationObserver(handleThemeChange);
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ['data-theme'],
+    });
+
+    // Inicializa el tema al montar el componente
+    handleThemeChange();
+
+    // Limpia el observador cuando el componente se desmonta
+    return () => {
+      observer.disconnect();
+    };
+  }, []); // Solo se ejecuta una vez al montar
 
   useEffect(() => {
     // Selecciona la imagen del logo utilizando un selector de clase
@@ -12,15 +34,15 @@ const CustomLogo: React.FC = () => {
     if (logoImg) {
       let logoSrc;
       if (location.pathname.startsWith('/docs/T1Comercios')) {
-        logoSrc = "/img/T1ComerciosLogo.svg";
+        theme === 'light' ? logoSrc = '/img/T1ComerciosLogo.svg' : logoSrc = '/img/T1ComerciosLogoDark.svg';
       } else if (location.pathname.startsWith('/docs/T1Pagos')) {
-        logoSrc = "/img/T1PagosLogos.svg";
+        theme === 'light' ? logoSrc = '/img/T1PagosLogos.svg' : logoSrc = '/img/T1PagosLogoDark.svg';
       } else {
-        logoSrc = "/img/t1envios-logo.svg";
+        theme === 'light' ? logoSrc = '/img/t1envios-logo.svg' : logoSrc = '/img/T1EnviosLogoDark.svg';
       }
       logoImg.src = logoSrc;
     }
-  }, [location.pathname]);
+  }, [location.pathname, theme]); // Ejecuta cuando cambia la ruta o el tema
 
   return null;
 };
